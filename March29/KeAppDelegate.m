7f0e5913cb8de219f5183f710f31bf3df2ae1735
Ke Wang <ke.wang@bayer.com>
2012-03-29 21:15:54 -0300

    Update March29/KeAppDelegate.m

--- March29/KeAppDelegate.m
+++ March29/KeAppDelegate.m
@@ -16,9 +16,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    UIScreen *screen = [UIScreen mainScreen];
+    view = [[View alloc] initWithFrame: screen.applicationFrame];
+    self.window = [[UIWindow alloc] initWithFrame: screen.bounds];
+    //self.window.backgroundColor = [UIColor whiteColor];
+
+    [self.window addSubview: view];
     [self.window makeKeyAndVisible];
     return YES;
 }
